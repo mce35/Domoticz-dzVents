83b3a54e887d67cb3ddb4808c3875ca3a0153ae3
Manuel CISSE <cisse.manuel@gmail.com>
2024-02-23 17:34:04 +0100

    Add geofence

--- alarm.lua
+++ alarm.lua
@@ -30,6 +30,16 @@ local devices_armed_home = {
     147  -- ZG porte garage
     }
 
+local devices_geofence = {
+    369, -- Tel1
+    370, -- Tel2
+}
+
+local all_devices = {}
+for k,v in pairs(devices_armed_away) do all_devices[#all_devices+1] = v end
+for k,v in pairs(devices_armed_home) do all_devices[#all_devices+1] = v end
+for k,v in pairs(devices_geofence) do all_devices[#all_devices+1] = v end
+
 local devices_trigger_time_sec = {
     ["default"] = 0,
     [205] = 30, -- ZG porte entrée
@@ -41,7 +51,7 @@ local devices_trigger_time_sec = {
     }
 
 local function has_value(tab, val)
-    for index, value in ipairs(tab) do
+    for index, value in pairs(tab) do
         if value == val then
             return true
         end
@@ -84,6 +94,24 @@ local function clear_alarm(domoticz)
     domoticz.devices(GW_ALARM_IDX).switchSelector(0) -- Gateway alarm ringtone
 end
 
+local function on_armed_away(domoticz, wait_time)
+    if(domoticz.security == "Armed Home")
+    then
+        domoticz.devices(SECURITY_IDX).disarm().silent()
+    end
+    domoticz.devices(SECURITY_IDX).cancelQueuedCommands()
+    if(wait_time > 0)
+    then
+        domoticz.devices(SECURITY_IDX).armAway().afterSec(wait_time)
+    else
+        domoticz.devices(SECURITY_IDX).armAway()
+    end
+    domoticz.devices(GW_LIGHT_IDX).setRGB(255, 0, 0)
+    domoticz.variables("alarm_trigger").cancelQueuedCommands()
+    domoticz.data.state = 0;
+    check_doors(domoticz)
+end
+
 local function on_armed_home(domoticz)
     clear_alarm(domoticz)
     check_doors(domoticz)
@@ -92,8 +120,37 @@ local function on_armed_home(domoticz)
     domoticz.data.state = 0;
 end
 
+local function on_geofence(domoticz, device)
+    domoticz.log("geofence=" ..  device.id .. ", state=" .. device.state, domoticz.LOG_DEBUG)
+    if(device.state == "On")
+    then
+        if(domoticz.security == "Armed Away")
+        then
+            domoticz.log("geofence=" ..  device.id .. " detected, deactivating alarm", domoticz.LOG_DEBUG)
+            domoticz.devices(SECURITY_IDX).cancelQueuedCommands() -- security
+            domoticz.devices(SECURITY_IDX).disarm()
+        end
+    else
+        local all_off = true
+        for index, value in pairs(devices_geofence) do
+            domoticz.log("check geofence=" ..  value, domoticz.LOG_DEBUG)
+            if(domoticz.devices(value).state == "On")
+            then
+                domoticz.log("geofence=" ..  value .. " still reachable, not activating alarm", domoticz.LOG_DEBUG)
+                all_off = false
+                break
+            end
+        end
+        if(all_off == true)
+        then
+            domoticz.log("All geofence devices are not reachable, activating alarm", domoticz.LOG_DEBUG)
+            on_armed_away(domoticz, 0)
+        end
+    end
+end
+
 return {
-    on = { devices = devices_armed_away,
+    on = { devices = all_devices,
         variables = { "alarm_trigger" },
         security = { domoticz.SECURITY_ARMEDAWAY, domoticz.SECURITY_ARMEDHOME, domoticz.SECURITY_DISARMED } },
     logging = { level = LOG_LEVEL, marker  = "Alarm" },
@@ -112,22 +169,16 @@ return {
                     domoticz.devices(SECURITY_IDX).disarm()
                 elseif(item.state == "1 Click")
                 then
-                    if(domoticz.security == "Armed Home")
-                    then
-                        domoticz.devices(SECURITY_IDX).disarm().silent()
-                    end
-                    domoticz.devices(SECURITY_IDX).cancelQueuedCommands()
-                    domoticz.devices(SECURITY_IDX).armAway().afterSec(300)
-                    domoticz.devices(GW_LIGHT_IDX).setRGB(255, 0, 0)
-                    domoticz.variables("alarm_trigger").cancelQueuedCommands()
-                    domoticz.data.state = 0;
-                    check_doors(domoticz)
+                    on_armed_away(domoticz, 300)
                 elseif(item.state == "2 Click")
                 then
                     domoticz.devices(SECURITY_IDX).cancelQueuedCommands() -- security
                     domoticz.devices(SECURITY_IDX).armHome().silent()
                     on_armed_home(domoticz)
                 end
+            elseif(has_value(devices_geofence, item.id))
+            then
+                on_geofence(domoticz, item)
             else
                 local device = item
                 if(device.state ~= "Off" and ((domoticz.security == "Armed Home" and has_value(devices_armed_home, device.id)) or domoticz.security == "Armed Away"))
